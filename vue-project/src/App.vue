9fe9a30eaae1c9481ee5bc4edb21405d67201df1
silviansi <silviansi24@gmail.com>
2025-01-01 21:05:30 +0700

    membuat komponen baru untuk props, emits, dan slots

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Student @response="showData" studentName="Silvia">Ini adalah nilai slot dari parent</Student> <br>
+  <Student @response="showData" studentName="Nanda" /> <br>
+  <Student @response="showData" studentName="Syafa" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>
+    Student Name: {{ studentName }}
+  </p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import Student from './components/Student.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  export default {
+    components: {
+      Student
+    },
+    data() {
+      return {
+        studentName: ''
+      }
+    },
+    methods: {
+      showData(data) {
+        this.studentName = data
+      }
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
---
+++ vue-project/src/components/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <slot>Ini adalah slot default karena parent komponen tidak memberikan data</slot> <br>
+    Ini adalah data dari siswa bernama {{ studentName }} <button @click="showData">Show Detail</button>
+</template>
+
+<script>
+export default {
+    props: {
+        studentName : String
+    },
+    emits: ['response'],
+    methods: {
+        showData() {
+            this.$emit('response', this.studentName)
+        }
+    }
+}
+</script>

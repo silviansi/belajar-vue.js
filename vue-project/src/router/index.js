f9becd7141a9c558cf2ebccfaebca4364214ca99
silviansi <silviansi24@gmail.com>
2025-01-03 19:59:24 +0700

    membuat nested routing

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -3,12 +3,35 @@ import DashboardView from "../views/Dashboard.vue";
 import AboutView from "../views/About.vue";
 import UserView from "../views/User.vue";
 import UserDetailView from "../views/UserDetail.vue";
+import UserProfileView from "../views/UserProfile.vue";
+import UserPostView from "../views/UserPost.vue";
+import UserIndexView from "../views/UserIndex.vue";
 
 const routes = [
     { path:'', component: DashboardView},
     { path:'/about', component: AboutView},
     { path: '/user', component: UserView },
-    { path: '/user/:id', component: UserDetailView }
+    // Routing with parameter
+    // { path: '/user/:id', component: UserDetailView },
+    // { path: '/user/:id/profile', component: UserProfileView },
+    // { path: '/user/:id/post', component: UserPostView },
+    // Routing nested
+    { path: '/user/:id', 
+        component: UserIndexView,
+        children: [
+            {
+                path: '',
+                component: UserDetailView
+            },
+            {
+                path: 'profile',
+                component: UserProfileView
+            },
+            {
+                path: 'post',
+                component: UserPostView
+            }
+        ]}
 ]
 
 const route = createRouter({
--- vue-project/src/views/UserDetail.vue
+++ vue-project/src/views/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
     Ini halaman detail user {{ $route.params.id }}
+    <br>
+    <RouterLink :to="'/user/'+$route.params.id+'/post'">{{ $route.params.id }}'s Posts'</RouterLink>
+    <br>
+    <RouterLink :to="'/user/'+$route.params.id+'/profile'">{{ $route.params.id }}'s Profile'</RouterLink>
 </template>
